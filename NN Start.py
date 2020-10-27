af3d431bcc826e014df57a593b92f1de444763d2
BazzahN <72982319+BazzahN@users.noreply.github.com>
2020-10-27 01:44:21 +0000

    Add files via upload

---
+++ NN Start.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 23:32:42 2020
+
+@author: harry
+"""
+
+import torch.nn as nn
+
+class Blank(nn.Module):
+    
+    def __init__(self):
+        
+    
+    def forward(self, t):
+        
+        return t
+        
+        
+    
+    
