acd2b2c1653c5c80f06a2f3913e18ef74cf926a1
BazzahN <72982319+BazzahN@users.noreply.github.com>
2020-10-27 15:05:57 +0000

    Add files via upload

--- NN.py
+++ NN.py
@@ -4,7 +4,18 @@ Created on Tue Oct 20 01:22:10 2020
 
 @author: harry
 """
+import torch
+
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchvision
+import torchvision.transforms as transforms
+
+torch.set_grad_enabled(True)
+torch.set_printoptions(linewidth=120)
+
 
 class Network(nn.Module):
 
@@ -24,7 +35,103 @@ class Network(nn.Module):
         #shion data set
     
     def forward(self,t):
+       # Input Layer
+       t=t
        
+       # COnv Layer
+       t = self.conv1(t)
+       t=F.relu(t)
+       t=F.max_pool2d(t, kernel_size=2, stride=2)
+       
+       t = self.conv2(t)
+       t=F.relu(t)
+       t=F.max_pool2d(t, kernel_size=2, stride=2)
+       
+       #Linear Layer
+       t = t.reshape(-1,12*4*4) #Flattens tensor
+       t = self.fc1(t)
+       t = F.relu(t)
+       
+       t = self.fc2(t)
+       t = F.relu(t)
+       
+       
+       #Output Layer
+       t = self.out(t)
+       #t = F.softmax(t,dim=1)
+       
+       return t
     
+
+def get_num_correct(preds,labels):
+    
+    return preds.argmax(dim=1).eq(labels).sum().item()
+
+
+
+network=Network() #Assigning network class
+
+train_set = torchvision.datasets.FashionMNIST(
+    root='./data/FashionMNIST'
+    ,train=True
+    ,download=True
+    ,transform=transforms.Compose([
+        transforms.ToTensor()
+    ])
+    ) # Data Extraction
+
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=100)#Data Loading
+optimizer = optim.Adam(network.parameters(), lr=0.01)
+
+
+batch = next(iter(train_loader)) #Assigning data to variable
+
+i = 0
+
+for epoch in range(5):
+    total_loss = 0
+    total_correct = 0
+    
+    for batch in train_loader:
+        images,labels = batch #Storing data in tuple
+    
+        preds = network(images)
+    
+        loss = F.cross_entropy(preds,labels) #Calculate Loss
         
-        return t
+        optimizer.zero_grad()
+        
+        loss.backward() #Calculates gradients difference between suc and loss
+        optimizer.step() #Update weights
+        
+        total_loss += loss.item()
+        total_correct += get_num_correct(preds,labels)
+    
+    
+
+    print("epock:",i,"total_correct:", total_correct, "loss:", total_loss)
+    i = i +1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   
