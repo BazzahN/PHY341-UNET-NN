dd7a298abc9dd3f45c6f02f54d6542795ff4d1fd
BazzahN <72982319+BazzahN@users.noreply.github.com>
2020-11-22 22:15:51 +0000

    Add files via upload

--- Image Loader.py
+++ Image Loader.py
@@ -4,26 +4,46 @@ Created on Sat Nov 21 19:34:45 2020
 
 @author: harry
 """
-
+import torch
+import torch.nn as nn
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
 def loadtiffs(file_name):
     img = Image.open(file_name)
-    print('The image is', img.size, 'Pixels.')
-    print ('With', img.n_frames, 'frames.')
     
     imgArray = np.zeros((img.size[1], img.size[0], img.n_frames), np.uint16)
+    
     for i in range(img.n_frames):
         img.seek(i)
         imgArray[:,:,i] = np.asarray(img)
     img.close()
-    return(imgArray)
+    return(imgArray, img.n_frames,img.size[1], img.size[0])
 
-epi_data = loadtiffs('/Users/harry/Documents/WORK/University/Physics/Third Year/PHY341-project/Training Images/epi.tif')
+epi_data, frames, h, w = loadtiffs(
+'/Users/harry/Documents/WORK/University/Physics/Third Year/PHY341-project/Training Images/epi.tif')
 
-phase_data = loadtiffs('/Users/harry/Documents/WORK/University/Physics/Third Year/PHY341-project/Training Images/phase.tif')
 
-plt.imshow(phase_data[:,:,2])
-plt.show()
+
+phase_data, frames, h, w = loadtiffs(
+'/Users/harry/Documents/WORK/University/Physics/Third Year/PHY341-project/Training Images/phase.tif')
+
+
+
+for i in range(frames):
+    
+    temp1 = phase_data[:,:,i]
+    norm = (temp1 - np.min(temp1)) / (np.max(temp1)-np.min(temp1))
+    norm_t = torch.from_numpy((norm[np.newaxis,np.newaxis]))
+    
+    
+    if i == 0:
+        norm_temp = norm_t
+        
+    else:
+        third = torch.cat((norm_temp,norm_t),0)
+        norm_temp = third
+
+
+
