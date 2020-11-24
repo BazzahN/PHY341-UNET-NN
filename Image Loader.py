8a72619fb4078f173c54fd298bf42cbead441446
BazzahN <72982319+BazzahN@users.noreply.github.com>
2020-11-24 14:49:09 +0000

    Add files via upload

--- Image Loader.py
+++ Image Loader.py
@@ -5,10 +5,10 @@ Created on Sat Nov 21 19:34:45 2020
 @author: harry
 """
 import torch
-import torch.nn as nn
+
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 def loadtiffs(file_name):
     img = Image.open(file_name)
@@ -19,32 +19,51 @@ def loadtiffs(file_name):
         img.seek(i)
         imgArray[:,:,i] = np.asarray(img)
     img.close()
-    return(imgArray, img.n_frames,img.size[1], img.size[0])
+    return(imgArray, img.n_frames)
 
-epi_data, frames, h, w = loadtiffs(
+
+
+
+
+
+epi_data, frames = loadtiffs(
 '/Users/harry/Documents/WORK/University/Physics/Third Year/PHY341-project/Training Images/epi.tif')
 
-
-
-phase_data, frames, h, w = loadtiffs(
+phase_data, frames = loadtiffs(
 '/Users/harry/Documents/WORK/University/Physics/Third Year/PHY341-project/Training Images/phase.tif')
 
-
+norm = []
 
 for i in range(frames):
     
-    temp1 = phase_data[:,:,i]
-    norm = (temp1 - np.min(temp1)) / (np.max(temp1)-np.min(temp1))
+  
+    norm = (phase_data[:,:,i] - np.min(phase_data[:,:,i])) / (np.max(phase_data[:,:,i])-np.min(phase_data[:,:,i]))
     norm_t = torch.from_numpy((norm[np.newaxis,np.newaxis]))
     
+
+    thresh = epi_data[:,:,i] > 5000
+    thresh = thresh.astype(int)
+    thresh_t = torch.from_numpy((thresh[np.newaxis,np.newaxis]))
     
     if i == 0:
         norm_temp = norm_t
+        thresh_temp = thresh_t
         
     else:
-        third = torch.cat((norm_temp,norm_t),0)
-        norm_temp = third
+        phase_t = torch.cat((norm_temp,norm_t),0)
+        norm_temp = phase_t
+        
+        epi_t = torch.cat((thresh_temp,thresh_t),0)
+        thresh_temp = epi_t
+        
+    
+epi_t = thresh_temp
+phase_t = norm_temp
 
-#Normalises phase data and transforms np array into a pytorch tensor
+
+
+
+
+#Normalises phase data, thresholds epi data and transforms np array into a pytorch tensor
 
 
